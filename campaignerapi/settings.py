dfd7ee18a55457eeb604c5054815de748455b243
Constantina Polycarpou <constantina.polycarpou@genomicsengland.co.uk>
2022-04-24 22:13:22 +0300

    chore: adds django-extensions for shell-plus

--- campaignerapi/settings.py
+++ campaignerapi/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "django_filters",
     "rest_framework",
     "corsheaders",
