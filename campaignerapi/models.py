46eb75766e6bdf197f78456ec27bbb2536e97478
Constantina <23738423+polyccon@users.noreply.github.com>
2022-04-24 20:49:54 +0300

    feat: adds messages model and migration

--- campaignerapi/models.py
+++ campaignerapi/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from django.db import (
+    models,
+    transaction,
+)
+from django.db.models import (
+    JSONField,
+    Q,
+)
+
 # Create your models here.
+
+
+class Messages(models.Model):
+    class Meta:
+        ordering = ("id",)
+
+    description = models.CharField(max_length=300, blank=True, null=True)
+    summary = JSONField(default=dict)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        editable=False,
+        related_name="messages",
+    )
