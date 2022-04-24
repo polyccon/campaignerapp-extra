674f6052dad200d374b284c54426f9c0cfe54959
Constantina <23738423+polyccon@users.noreply.github.com>
2022-04-24 13:31:30 +0300

    feat: improve config setup loading env vars in docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,47 @@
-FROM python:alpine as base
+ARG DOCKER_IMAGE_BASE=python:3.8.10-slim
+FROM ${DOCKER_IMAGE_BASE} as base_python
+
+# Virtualenv setup ------------------------------------------------------------
+
+FROM base_python as venv
+
+RUN \
+    apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    libpq-dev \
+    zlib1g-dev \
+    autoconf \
+    automake \
+    make \
+    gcc \
+    perl \
+    libbz2-dev \
+    liblzma-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    && \
+    pip install --no-cache-dir virtualenv==20.0.18 setuptools==44.1.0 &&\
+    true
+
+RUN virtualenv /opt/venv
+
+ENV PATH="/opt/venv/bin:$PATH"
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt --no-binary psycopg2
+
+
+# Main container --------------------------------------------------------------
+
+FROM base_python as base
+
+RUN \
+    apt-get update && apt-get install -y --no-install-recommends \
+    libpq-dev libcurl4-openssl-dev &&\
+    true
+
+# Copy python packages
+COPY --from=venv /opt/venv /opt/venv
 
 ENV PATH="/opt/venv/bin:$PATH"
 
@@ -6,6 +49,6 @@ WORKDIR /code
 
 COPY requirements.txt .
 
-RUN pip3 install -r requirements.txt
+RUN pip install -r requirements.txt
 
 EXPOSE 5500
--- campaignerapi/settings.py
+++ campaignerapi/settings.py
@@ -12,16 +12,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from celery.schedules import crontab
 from pathlib import Path
+from decouple import (
+    AutoConfig,
+    Config,
+    RepositoryEnv,
+)
+from campaignerapi.config.fallbacks import SettingsFallbacks
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+settings_location = os.getenv("SETTINGS_LOCATION")
+if settings_location is not None:
+    config = Config(RepositoryEnv(settings_location))
+else:
+    config = AutoConfig()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r2+b_crtb(+ieioynq0&w=v2()7+)tj5lxva0m(jy(ceb9*_q0'
+# SECRET_KEY = "test"
+SECRET_KEY = config("SECRET_KEY", default=SettingsFallbacks.SECRET_KEY.value)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,12 +44,12 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "django_filters",
     "rest_framework",
     "campaignerapi.apps.WebservicesConfig",
@@ -46,74 +58,74 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # "app.middleware.logger.LoggingMiddleware",
 ]
 
-ROOT_URLCONF = 'campaignerapi.urls'
+ROOT_URLCONF = "campaignerapi.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'campaignerapi.wsgi.application'
+WSGI_APPLICATION = "campaignerapi.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_TIMEOUT=100
-EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL=os.getenv("DEFAULT_FROM_EMAIL")
-EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
+EMAIL_TIMEOUT = 100
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-POSTGRES_HOST_AUTH_METHOD="trust"
+POSTGRES_HOST_AUTH_METHOD = "trust"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -121,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -135,25 +147,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CELERY_BEAT_SCHEDULE = {
-      'send-email-every-30-seconds': {
-        'task': "campaignerapi.tasks.test_task_email.send_email_task",
-        'schedule': 30.0,
-        # 'args': (16, 16),
-        'options': {
-            'expires': 15.0,
-        },
-    },
-}
+# CELERY_BEAT_SCHEDULE = {
+#       'send-email-every-30-seconds': {
+#         'task': "campaignerapi.tasks.test_task_email.send_email_task",
+#         'schedule': 30.0,
+#         # 'args': (16, 16),
+#         'options': {
+#             'expires': 15.0,
+#         },
+#     },
+# }
 
-CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_BROKER_URL = "amqp://localhost"
 
 CORS_ALLOW_HEADERS = (
     "x-requested-with",
