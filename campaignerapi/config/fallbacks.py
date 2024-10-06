50d647a5ff61ee5ae3d6821ca407022d1415d94c
polyccon <polyccon@gmail.com>
2024-10-06 17:18:39 +0100

    chore: add database secrets into fallbacks

--- campaignerapi/config/fallbacks.py
+++ campaignerapi/config/fallbacks.py
@@ -5,3 +5,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class SettingsFallbacks(Enum):
     SECRET_KEY = "this-is-not-a-secret-key"
+    DATABASE_HOST = "db"
+    DATABASE_NAME = "campaignerapi"
+    DATABASE_PASSWORD = "not-a-password"
+    DATABASE_USER = "not-a-user"
+    DATABASE_PORT = 5432
+    DATABASE_SSL = False
+    DATABASE_ENGINE = "django.db.backends.postgresql"
